5d55dca5d2753447315853420ea3abba8afd99c3
Eliya Gervas <eliyagervas27@gmail.com>
2022-07-14 20:39:54 +0300

    nav bar changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
 import React from "react";
+import {
+  Container,
+  Stack,
+  AppBar,
+  Typography,
+  Box,
+  Avatar,
+} from "@mui/material";
 function App() {
   return (
-    <div className="App">
-      <h2>Hello</h2>
-    </div>
+    <Container maxWidth="xl">
+      <AppBar position="static">
+        <Stack
+          display="flex"
+          flexDirection="horizontal"
+          justifyContent="space-between"
+        >
+          <Box>
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="/"
+              sx={{
+                mr: 2,
+                display: { xs: "none", md: "flex" },
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              Let'sTalk🤠
+            </Typography>
+          </Box>
+
+          <Box>
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+          </Box>
+        </Stack>
+      </AppBar>
+    </Container>
   );
 }
 
