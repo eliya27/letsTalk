232d31b81278f48244d028dec9f5b63adde10f9c
Eliya Gervas <eliyagervas27@gmail.com>
2022-07-14 17:32:40 +0300

    new changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          LetsTalk <code>App</code> is awesome.
+          LetsTalk <code>App</code> is awesome really awesome.
         </p>
         <a
           className="App-link"
